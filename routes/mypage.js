74f0d60eef64d2542850fdf4483669389ca9d61e
UmarFaruqueN <umarfaruquen@gmail.com>
2022-01-12 11:01:51 +0530

    third one

--- routes/mypage.js
+++ routes/mypage.js
@@ -1,38 +1,42 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-const values = ['umar','faruque','N']
-var data=[
+router.get("/", function (req, res, next) {
+    const values = ["umar", "faruque", "N"];
+    var data = [
+        {
+            name: "DressBerry",
+            image: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/2278228/2018/4/5/11522913267066-DressBerry-Women-Watches-7501522913266942-1.jpg",
+            description: "Woman Analouge Watch",
+            price: " Rs : 929 /-",
+        },
+        {
+            name: "DressBerry",
+            image: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/2278228/2018/4/5/11522913267066-DressBerry-Women-Watches-7501522913266942-1.jpg",
+            description: "Woman Analouge Watch",
+            price: " Rs : 929 /-",
+        },
+        {
+            name: "DressBerry",
+            image: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/2278228/2018/4/5/11522913267066-DressBerry-Women-Watches-7501522913266942-1.jpg",
+            description: "Woman Analouge Watch",
+            price: " Rs : 929 /-",
+        },
+        {
+            name: "DressBerry",
+            image: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/2278228/2018/4/5/11522913267066-DressBerry-Women-Watches-7501522913266942-1.jpg",
+            description: "Woman Analouge Watch",
+            price: " Rs : 929 /-",
+        },
+    ];
+    const sample = { name: "umar", comments: { comment: "sample cooment", date: "12/122/222" }, admin: false };
 
-{
-  name: "DressBerry",
-  image: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/2278228/2018/4/5/11522913267066-DressBerry-Women-Watches-7501522913266942-1.jpg",
-  description: "Woman Analouge Watch",
-  price: " Rs : 929 /-",
-},
-{
-  name: "DressBerry",
-  image: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/2278228/2018/4/5/11522913267066-DressBerry-Women-Watches-7501522913266942-1.jpg",
-  description: "Woman Analouge Watch",
-  price: " Rs : 929 /-",
-},
-{
-  name: "DressBerry",
-  image: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/2278228/2018/4/5/11522913267066-DressBerry-Women-Watches-7501522913266942-1.jpg",
-  description: "Woman Analouge Watch",
-  price: " Rs : 929 /-",
-},
-{
-  name: "DressBerry",
-  image: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/2278228/2018/4/5/11522913267066-DressBerry-Women-Watches-7501522913266942-1.jpg",
-  description: "Woman Analouge Watch",
-  price: "929",
-},
-];
-    const sample ={ name:'umar',comments:{ comment:"sample cooment", date: "12/122/222"}, admin:false}
-  res.render('mypage', {values,data});
+    if (req.session.loggedIn) {
+        res.render("mypage", { values, data });
+    } else {
+        res.redirect("/");
+    }
 });
 
 module.exports = router;
